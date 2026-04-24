ee25cbb2f2a6b0309edc91c37e219807debe9312
Lukáš Hejtmánek <hejtmy@gmail.com>
2026-04-24 20:26:39 +0200

    Initializing gh-pages branch

---
+++ README.md
@@ -0,0 +1,1 @@
+# semilongforest-analysis
---
+++ main.R
@@ -0,0 +1,46 @@
+library(psychotronr)
+library(tidyr)
+library(ggplot2)
+source("functions/processing.R")
+
+raw <- jsonlite::fromJSON("data/study_export.json")
+data <- organize_psychotron_data(raw)
+organized <- split_participant_email(data, drop_email = TRUE)
+surveys <- organized$surveys
+names(surveys)
+
+poms_1 <- surveys[["1-poms-sf-pre"]]
+poms_1$data
+ros_1 <- surveys[["1-ros-pre"]]
+ros_1$data
+ssq_data <- surveys[["2-ssq"]]$data
+
+# unnest the poms1_data_poms colum, which is a matrix using regular r without dplyr
+organize
+df_poms <- process_all_poms(organized$surveys) %>%
+  select(session_order, participant, timepoint, 
+         anger, vitality, fatigue, depression, confusion, tension) %>%
+  pivot_longer(cols = c(anger, vitality, fatigue, depression, confusion, tension), 
+               names_to = "scale", values_to = "score")
+df_ros <- process_all_ros(organized$surveys) %>%
+  select(session_order, participant, timepoint, ros_score)
+df_ssq <- process_all_ssq(organized$surveys) %>%
+  select(session_order, participant, ssq_score, vrsq_oculomotor, vrsq_disorientation, vrsq_total)
+
+df_ros %>%
+  pivot_wider(names_from = timepoint, values_from = ros_score) %>%
+  mutate(change = post - pre) %>%
+  ggplot(aes(x = session_order, y = change, 
+    color = participant, group = participant)) +
+  geom_point() +
+  geom_line()
+
+# 
+df_poms %>%
+  pivot_wider(names_from = timepoint, values_from = score) %>%
+  mutate(change = post - pre) %>%
+  ggplot(aes(x = session_order, y = change, 
+             color = participant, group = participant)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~scale)
